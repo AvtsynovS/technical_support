94a8bd69e42907ccf4f3a2b82c5842285fedbd8d
Sergey <avtsynov.s@yandex.ru>
2022-05-30 00:58:02 +0300

    add Footer component

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Icon } from '../SvgIcons/Icons';
+import styles from './styles.module.scss';
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <Icon iconName='footer' />
+    </div>
+  );
+};
+
+export default Footer;
