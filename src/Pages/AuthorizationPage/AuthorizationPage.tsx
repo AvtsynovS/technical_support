193798b846ba507d2d7f40d249b1a1733bce0bfd
Sergey <avtsynov.s@yandex.ru>
2022-05-29 13:29:57 +0300

    add Button UI

--- src/Pages/AuthorizationPage/AuthorizationPage.tsx
+++ src/Pages/AuthorizationPage/AuthorizationPage.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Button } from '../../UI/Button/Button';
 // import styles from './styles.module.scss';
 
 const AuthorizationPage: React.FC = () => {
-  return <div>AuthorizationPage</div>;
+  return (
+    <div>
+      <div>AuthorizationPage</div>
+      <Button bgColor='secondary' color='secondary'>
+        Secondary Button
+      </Button>
+      <Button bgColor='prymary'>Prymary Button</Button>
+      <Button>Warning Button</Button>
+    </div>
+  );
 };
 
 export default AuthorizationPage;
---
+++ src/UI/Button/Button.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled from 'styled-components';
+import { color } from '../../Constants';
+
+interface Props {
+  children: string;
+  bgColor?: color;
+  color?: string;
+}
+
+const StyleButton = styled.button<Props>`
+  padding: 8px 16px;
+  border-radius: 16px;
+  font: 700 18px/36px 'Inter';
+  letter-spacing: 0.25px;
+  border: 1px solid #adadad;
+  box-sizing: border-box;
+  color: ${(props) => {
+    if (props.color === 'secondary') {
+      return '#858585';
+    }
+    return '#fff';
+  }};
+  background-color: ${(props) => {
+    if (props.bgColor === 'prymary') {
+      return '#7db59a';
+    } else if (props.bgColor === 'secondary') {
+      return 'transparent';
+    }
+    return '#e84393';
+  }};
+
+  &: hover {
+    color: ${(props) => {
+      if (props.color === 'secondary') {
+        return '#7DB59A';
+      }
+      return '#fff';
+    }};
+    background-color: ${(props) => {
+      if (props.bgColor === 'prymary') {
+        return '#5B9479';
+      } else if (props.bgColor === 'secondary') {
+        return 'transparent';
+      }
+      return '#B03671';
+    }};
+  }
+
+  &: active {
+    color: ${(props) => {
+      if (props.color === 'secondary') {
+        return '#4F896C';
+      }
+      return '#fff';
+    }};
+    background-color: ${(props) => {
+      if (props.bgColor === 'prymary') {
+        return '#4F896C';
+      } else if (props.bgColor === 'secondary') {
+        return 'transparent';
+      }
+      return '#832552';
+    }};
+  }
+`;
+
+export const Button = (props: Props) => {
+  const { bgColor, children, color } = props;
+  return (
+    <StyleButton color={color} bgColor={bgColor}>
+      {children}
+    </StyleButton>
+  );
+};
