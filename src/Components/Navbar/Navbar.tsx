311cde4ab20d7ab5ca98f2aa07d1b6958e89e197
Sergey <avtsynov.s@yandex.ru>
2022-06-05 02:10:00 +0300

    edit navbar style

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Icon } from '../SvgIcons/Icons';
 import styles from './styles.module.scss';
+import navbarBrand from '../../assets/images/navbarBrand.png';
 // import { Routes, Route, Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
-      <Icon iconName='white-brand' />
-      <ul className={styles.navbarList}>
+      <img src={navbarBrand} alt='brand' />
+      <ul className={styles.navbar__list}>
         <li>
           <Icon iconName='icon-home' />
         </li>
